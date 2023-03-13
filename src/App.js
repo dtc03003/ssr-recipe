fc3b74314e08e3629c60c8ebe6cf29d957433cdb
김동현 <dtc03003@naver.com>
2023-03-13 23:48:07 +0900

    서버 사이드 렌더링 구현 준비

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
+import { Route, Routes } from 'react-router-dom';
+import Menu from './components/Menu';
+import RedPage from './pages/RedPage';
+import BluePage from './pages/BluePage';
+
 function App() {
   return (
-    <div className="App">
-
+    <div>
+      <Menu />
+      <hr />
+      <Routes>
+        <Route path="/blue" element={<BluePage />} />
+        <Route path="/red" element={<RedPage />} />
+      </Routes>
     </div>
   );
 }
